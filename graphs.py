fb636049a40f954b8e0a869a183a203a0349b1bb
lvparijs <vanparijslotte@gmail.com>
2022-12-19 00:06:17 +0100

    veralgemening

--- graphs.py
+++ graphs.py
@@ -5,17 +5,18 @@ import matplotlib.pyplot as plt
 
 csvreader = pandas.read_csv("./compare/score.csv")
 
-csv_NSK = csvreader[csvreader['algo'] == "MILboost"]
+algos = ["MILboost LogSumExponential", "MILboost", "MissSVM", "sbMIL", "sbMIL", "SIL", "STK", "NSK", "MICA"]
+for algo in algos:
+    csv_algo = csvreader[csvreader['algo'] == algo]
 
-for dataset in ['fox','mutagenesis-atoms','mutagenesis-bonds','mutagenesis-chains','eastWest','elephant','tiger','westEast', 'musk1']:
-    csv_NSK_dataset = csv_NSK[csv_NSK['dataset'] == dataset]
+    for dataset in ['fox','mutagenesis-atoms','mutagenesis-bonds','mutagenesis-chains','eastWest','elephant','tiger','westEast', 'musk1']:
+        csv_algo_dataset = csv_algo[csv_algo['dataset'] == dataset]
 
-    plt.scatter(csv_NSK_dataset['k_fold'], csv_NSK_dataset['acc'], label=dataset)
+        plt.scatter(csv_algo_dataset['k_fold'], csv_algo_dataset['acc'], label=dataset)
 
-plt.title("MILBOOST")
-plt.xlabel("k-fold")
-plt.ylabel("acc")
-plt.legend(bbox_to_anchor=(1.025, 1), loc='upper left', borderaxespad=0)
-plt.show()
-
-print(csv_NSK)
+    plt.title(algo)
+    plt.xlabel("k-fold")
+    plt.ylabel("acc")
+    plt.legend(bbox_to_anchor=(1.025, 1), loc='upper left', borderaxespad=0)
+    plt.savefig(algo)
+    plt.close()
