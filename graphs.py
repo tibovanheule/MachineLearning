37badeba4532808de723dd1d21c48c7e28d117b8
lvparijs <vanparijslotte@gmail.com>
2022-12-19 09:54:53 +0100

    make y axis go to 1

--- graphs.py
+++ graphs.py
@@ -17,6 +17,7 @@ for algo in algos:
     plt.title(algo)
     plt.xlabel("k-fold")
     plt.ylabel("acc")
+    plt.ylim(0,1)
     plt.legend(bbox_to_anchor=(1.025, 1), loc='upper left', borderaxespad=0)
     plt.savefig(algo, bbox_inches='tight')
     plt.close()
