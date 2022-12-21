c1529f76f894e9e6dbdec426c342f49509b4211b
Tibo Vanheule <tibo@tibovanheule.space>
2022-12-21 17:41:26 +0100

    take in account the pc (added name to csv)

--- score.py
+++ score.py
@@ -7,6 +7,7 @@ import pandas as pd
 from sklearn.metrics import confusion_matrix
 import seaborn as sns
 import numpy as np
+import platform
 
 def result(algo,y_true,y_pred,time_ep,k_fold,dataset):
     epoch_time = int(time.time())
@@ -21,25 +22,8 @@ def result(algo,y_true,y_pred,time_ep,k_fold,dataset):
     plt.savefig(str("compare/" + str(epoch_time)), dpi=300)
     plt.clf()
 
-    conf_matrix = confusion_matrix(y_true=y_true, y_pred=y_pred)
-    
-    #
-    # Print the confusion matrix using Matplotlib
-    #
-    #fig, ax = plt.subplots(figsize=(7.5, 7.5))
-    #ax.matshow(conf_matrix, cmap=plt.cm.Blues, alpha=0.3)
-    #for i in range(conf_matrix.shape[0]):
-    #    for j in range(conf_matrix.shape[1]):
-    #        ax.text(x=j, y=i,s=conf_matrix[i, j], va='center', ha='center', size='xx-large')
-
-    #plt.xlabel('Predictions', fontsize=18)
-    #plt.ylabel('Actuals', fontsize=18)
-    #plt.title('Confusion Matrix for ' + algo + 'on dataset ' + dataset, fontsize=18)
-    #plt.savefig(str("compare/"+str(epoch_time)),dpi=300)
-    #plt.clf()
-
     report = classification_report(y_true=y_true, y_pred=y_pred,output_dict=True)
-    names = ["algo", "k_fold", "time_running","time","acc","dataset"]
+    names = ["algo", "k_fold", "time_running","time","acc","dataset", "pc"]
     acc = report["accuracy"]
     del report["accuracy"] 
     names += [ up +"_" + lo for up in report for lo in report[str(up)]]
@@ -49,7 +33,7 @@ def result(algo,y_true,y_pred,time_ep,k_fold,dataset):
         with open(file_path,"a") as file:
             file.write(",".join(names)+"\n")
     with open(file_path,"a") as file:
-        result = [algo, str(k_fold), str(epoch_time),str(time_ep),str(acc),dataset]
+        result = [algo, str(k_fold), str(epoch_time),str(time_ep),str(acc),dataset,platform.node()]
         result += [ str(report[upper][lo]) for upper in report for lo in report[upper]]
         file.write(",".join(result)+"\n")
         
