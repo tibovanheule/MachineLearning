e12ff4e4228ebaea3f7f89252120fdb69ed23502
Tibo Vanheule <tibo@tibovanheule.space>
2022-12-23 04:35:31 +0100

    graphs notebook

--- graphs.py
+++ graphs.py
@@ -3,14 +3,12 @@
 import pandas
 import matplotlib.pyplot as plt
 
-csvreader = pandas.read_csv("./compare/score.csv")
+data = pandas.read_csv("./compare/score.csv")
 
-algos = ["MILboost LogSumExponential", "MILboost", "MissSVM", "sbMIL", "sbMIL", "SIL", "STK", "NSK", "MICA"]
+algos = set(data.algo)
 for algo in algos:
-    csv_algo = csvreader[csvreader['algo'] == algo]
-
-    for dataset in ['fox','mutagenesis-atoms','mutagenesis-bonds','mutagenesis-chains','eastWest','elephant','tiger','westEast', 'musk1', 'musk2']:
-        csv_algo_dataset = csv_algo[csv_algo['dataset'] == dataset]
+    for dataset in set(data.dataset):
+        csv_algo_dataset = data[data['dataset'] == dataset & data['algo'] == algo]
 
         accuracies = csv_algo_dataset['acc'].values.tolist()
         kfold = csv_algo_dataset['k_fold'].values.tolist()
@@ -22,7 +20,7 @@ for algo in algos:
             accs[ (kfold[i] - 1) % 5 ] += acc
             howmany[ (kfold[i] - 1) % 5 ] += 1
 
-        if(not howmany.__contains__(0)):
+        if 0 not in howmany:
             res = [i / j for i, j in zip(accs, howmany)]
             plt.scatter([1,2,3,4,5], accs, label=dataset)
 
